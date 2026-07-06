7baf2fb975cefa8df005e50d2fe7b9922aec19cc
LeoElliottWeb <sales@noirsoft.net>
2026-07-06 09:40:05 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    // 👇 CHANGE THIS TO YOUR EXACT GITHUB REPOSITORY NAME
     base: '/Kohinoor/',
-
     plugins: [react()]
 })
