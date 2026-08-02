9f85f0b52c67e3f4a8d568f317e64b434734584b
LeoElliottWeb <sales@noirsoft.net>
2026-08-02 15:19:19 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 ﻿import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+import Stats from './Stats.jsx' // Import your Stats component
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        {/* The main route loads your chat app */}
+        <Route path="/" element={<App />} />
+        
+        {/* The /stats route loads your Stats page */}
+        <Route path="/stats" element={<Stats />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
 )
